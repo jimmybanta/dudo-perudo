6e62cc70439a816728a4ea022bb580733cab34a1
jimmybanta <jimmy.banta24@gmail.com>
2024-01-19 13:56:38 +0100

    moved perudo directory into backend

---
+++ backend/perudo/hand.py
@@ -0,0 +1,30 @@
+
+from dice import D6
+
+
+class Hand(list):
+    def __init__(self, *args, size=5):
+        if args:
+            if len(args) != size:
+                raise ValueError('Need to pass in {} values!'.format(size))
+            else:
+                for arg in args:
+                    self.append(D6(value=arg))
+        else:
+            for _ in range(size):
+                self.append(D6())
+        self.sort()
+
+
+        l = ['ones', 'twos', 'threes', 'fours', 'fives', 'sixes']
+
+        for i, attr in enumerate(l):
+            setattr(self, attr, self.count(i + 1))
+
+
+    def __str__(self):
+        return '{}'.format([die.value for die in self])
+    
+    def count(self, num):
+        d = [die.value for die in self]
+        return d.count(num)
